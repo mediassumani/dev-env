cfc0bf9450615a4952175f3f42d11f8b170f0b35
Medi Assumani <mediassumani49@gmail.com>
2019-11-09 19:10:04 -0800

    Create medi_env.sh

---
+++ medi_env.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+brew cask install visual-studio-code
+
+# Genral Tools
+
+brew install git
+
+# iOS Development
+
+xcode-select --install
+sudo gem install cocoapods
+brew install swiftlint
+sudo gem install fastlane -NV
+
+# Backend Services Installation
+
+brew install node
+brew install npm
+brew install mongodb
+
+
+
