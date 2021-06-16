be5d0cc46cd6d54734b8cc38c83c6da37e3525fc
Medi Assumani <mediassumani49@gmail.com>
2021-06-16 17:13:01 -0400

    Update and rename medi_env.sh to config.sh

--- config.sh
+++ config.sh
@@ -3,13 +3,25 @@
 # Pre-install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-# General Tools
-
+# Internal Tools
 brew install git
 brew install zsh zsh-completions
+
+# IDEs
+brew cask install visual-studio-code
+mas install 497799835 # Installs xcode from the Mac AppStore
+mas install 585829637 # Todoist
+mas install 1147396723 # Whastapp
+brew install --cask notion # Notion
+brew install --cask slack # Slack
+brew install --cask github # Github Desktop
+
+ 
 brew cask install visual-studio-code
 brew cask install postman
 brew cask install iterm2
+brew install mas
+brew link mas
 
 # iOS Development
 
