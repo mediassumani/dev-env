fb8231fbef52ae6323dfbbcf5246aec3da9218ee
Medi Assumani <mediassumani49@gmail.com>
2021-06-20 21:17:22 -0400

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,24 +4,25 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # Internal Tools
+# Set-up
 brew install git
 brew install zsh zsh-completions
+brew install mas
+brew link mas
 
-# IDEs
-brew cask install visual-studio-code
-mas install 497799835 # Installs xcode from the Mac AppStore
+# Mac Applications
 mas install 585829637 # Todoist
 mas install 1147396723 # Whastapp
+mas install 497799835 # Xcode
+
 brew install --cask notion # Notion
 brew install --cask slack # Slack
 brew install --cask github # Github Desktop
+brew install --cask sourcetree # SourceTree
+brew install --cask visual-studio # Visual Studio
+brew cask install iterm2 # Iterm2
+brew cask install postman # Postman
 
- 
-brew cask install visual-studio-code
-brew cask install postman
-brew cask install iterm2
-brew install mas
-brew link mas
 
 # iOS Development
 
@@ -30,10 +31,6 @@ sudo gem install cocoapods
 brew install swiftlint
 sudo gem install fastlane -NV
 
-# Backend Services Installation
-
+# Backend Development
 brew install node
 brew install npm
-
-
-
