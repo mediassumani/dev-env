909a30cdcf69fb5594708c59859eb8987d0a4713
Medi Assumani <mediassumani49@gmail.com>
2022-06-22 11:26:04 -0400

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,25 +3,21 @@
 # Pre-install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-# Internal Tools
-# Set-up
+# Internal ToolsSet-up
 brew install git
 brew install zsh zsh-completions
 brew install mas
 brew link mas
 
 # Mac Applications
-mas install 585829637 # Todoist
 mas install 1147396723 # Whastapp
 mas install 497799835 # Xcode
-
-brew install --cask notion # Notion
-brew install --cask slack # Slack
-brew install --cask github # Github Desktop
-brew install --cask sourcetree # SourceTree
-brew install --cask visual-studio # Visual Studio
-brew cask install iterm2 # Iterm2
-brew cask install postman # Postman
+brew install --cask notion
+brew install --cask slack
+brew install --cask github
+brew install --cask visual-studio
+brew cask install iterm2
+brew cask install postman
 
 
 # iOS Development
